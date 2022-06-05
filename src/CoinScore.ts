8124935fe27863d270bd091b09b7798e33a722d0
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-06-05 13:11:23 +0100

    CoinScore as a map of different scores

--- src/CoinScore.ts
+++ src/CoinScore.ts
@@ -1,31 +1,67 @@
+import { ScoreSelectivity } from "./Types";
+
 export class CoinScore {
   private readonly n: string;
+  /**
+   * The map of score value for each selectivity level.
+   */
+  private readonly sm: {
+    [ScoreSelectivity.EXTREME]: number;
+    [ScoreSelectivity.HIGH]: number;
+    [ScoreSelectivity.MODERATE]: number;
+    [ScoreSelectivity.MINIMAL]: number;
+  } = {
+    [ScoreSelectivity.EXTREME]: 0,
+    [ScoreSelectivity.HIGH]: 0,
+    [ScoreSelectivity.MODERATE]: 0,
+    [ScoreSelectivity.MINIMAL]: 0,
+  };
+
+  /**
+   * @deprecated
+   */
   private r = 0;
 
   constructor(coinName: string, obj?: CoinScore) {
     this.n = coinName;
+    this.sm = obj?.sm ?? this.sm;
     this.r = obj?.r ?? this.r;
   }
 
   static fromObject(obj: object): CoinScore {
-    return Object.assign(Object.create(CoinScore.prototype), obj);
+    return Object.assign(new CoinScore(``), obj);
   }
 
-  /**
-   * The number of times this coin was going up when the rest of the market wasn't.
-   */
-  get score(): number {
-    return this.r;
+  getScore(selectivity: ScoreSelectivity): number {
+    return this.sm[selectivity];
   }
 
   get coinName(): string {
     return this.n;
   }
 
+  addScore(selectivity: ScoreSelectivity, value: number): void {
+    this.sm[selectivity] += value;
+  }
+
+  /**
+   * The number of times this coin was going up when the rest of the market wasn't.
+   * @deprecated
+   */
+  get score(): number {
+    return this.r;
+  }
+
+  /**
+   * @deprecated
+   */
   scoreUp(): void {
     this.r++;
   }
 
+  /**
+   * @deprecated
+   */
   scoreDown(): void {
     this.r > 0 && this.r--;
   }
