8968e63eb0c894b53f459b97b2bbefa6d4153a86
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2022-06-10 11:51:30 +0100

    Minified score map

--- src/CoinScore.ts
+++ src/CoinScore.ts
@@ -1,19 +1,25 @@
-import { ScoreSelectivityKeys } from "./Types";
+import { ScoreSelectivity, ScoreSelectivityKeys } from "./Types";
+import { enumKeys } from "./Functions";
 
-export type ScoreValueMap = {
-  [key in ScoreSelectivityKeys]: number;
+const shortMapping: { [key in ScoreSelectivityKeys]: ShortMappingKeys } = {
+  EXTREME: `E`,
+  HIGH: `H`,
+  MODERATE: `M`,
+  MINIMAL: `MI`,
 };
+
+type ShortMappingKeys = `E` | `H` | `M` | `MI`;
 
 export class CoinScore {
   private readonly n: string;
   /**
    * The map of score value for each selectivity level.
    */
-  private readonly sm: ScoreValueMap = {
-    EXTREME: 0,
-    HIGH: 0,
-    MODERATE: 0,
-    MINIMAL: 0,
+  private readonly sm: { [key in ShortMappingKeys]: number } = {
+    E: 0,
+    H: 0,
+    M: 0,
+    MI: 0,
   };
 
   constructor(coinName: string, obj?: any) {
@@ -26,9 +32,14 @@ export class CoinScore {
     cs: CoinScore,
     key: ScoreSelectivityKeys
   ): void {
-    if (old.r) {
+    if (old?.r) {
       cs.sm[key] = old.r;
     }
+    enumKeys<ScoreSelectivityKeys>(ScoreSelectivity).forEach((k) => {
+      cs.sm[shortMapping[k]] = old?.[k] ?? 0;
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete cs.sm[k];
+    });
   }
 
   static fromObject(obj: object): CoinScore {
@@ -36,7 +47,7 @@ export class CoinScore {
   }
 
   getScore(key: ScoreSelectivityKeys): number {
-    return this.sm ? this.sm[key] : 0;
+    return this.sm ? this.sm[shortMapping[key]] : 0;
   }
 
   get coinName(): string {
@@ -44,6 +55,6 @@ export class CoinScore {
   }
 
   addScore(key: ScoreSelectivityKeys, value: number): void {
-    this.sm[key] += value;
+    this.sm[shortMapping[key]] += value;
   }
 }
